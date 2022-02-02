a70796dd0b188841e0b03a163e51e9197cb1ffde
mukhlif <asaadmohamad39@gmail.com>
2022-02-02 20:01:20 +0200

    new project

---
+++ src/guess/guess.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package guess;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class guess {
+    public int score=0;
+    public String answer;
+    public guess(int s,String a){
+    this.score=s;
+    this.answer=a;
+    }
+    public static void main(String args[]){
+    
+    
+    }
+
+    public guess() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
